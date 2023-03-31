1d05df719418854895ff3cedbd29f13dd79a6c75
Chlupus <purkertovam@gmail.com>
2023-03-31 15:58:01 +0200

    Program test

--- OwlKitty/OwlKitty/OwlKittyOffspring.cs
+++ OwlKitty/OwlKitty/OwlKittyOffspring.cs
@@ -8,5 +8,38 @@ namespace OwlKitty
 {
     internal class OwlKittyOffspring
     {
+        IKitty kitty;
+        IOwl owl;
+
+       OwlKittyOffspring()
+        {
+            owl = new Owl();
+            kitty = new Kitty();
+        }
+
+        public string Fluffy()
+        {
+            return kitty.Fluffy();
+        }
+
+        public int LegNumber()
+        {
+            return kitty.LegNumber();
+        }
+
+        public string Feathers()
+        {
+            return owl.Feathers();
+        }
+
+        public int Wings()
+        {
+            return owl.Wings();
+        }
+
+        public string Nature()
+        {
+            return owl.Nature();
+        }
     }
 }
