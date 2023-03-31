32c0138d67787bedabc2532592f7ecba8db84902
Michaela-Purkert <129502870+Michaela-Purkert@users.noreply.github.com>
2023-03-31 20:10:04 +0200

    Revert "Development"

--- OwlKitty/OwlKitty/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using OwlKitty;
-using System.Net.Sockets;
-
-OwlKittyOffspring owlKitty = new OwlKittyOffspring();
-
-Console.WriteLine("Our owlkitty is in the end {0}, {1} and {2}. He has {3} legs and {4} wings.",
-    owlKitty.Fluffy(), owlKitty.Feathers(), owlKitty.Nature(), owlKitty.LegNumber(), owlKitty.Wings());
-Console.WriteLine("One top notch chaos ball :)");
