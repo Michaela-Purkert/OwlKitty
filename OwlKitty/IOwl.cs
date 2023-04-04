5580a831d254e87798b35277be7cdade05f38528
Michaela-Purkert <129502870+Michaela-Purkert@users.noreply.github.com>
2023-04-04 11:40:42 +0200

    project Owlkitty (#3)
    Co-authored-by: Chlupus <purkertovam@gmail.com>

---
+++ OwlKitty/IOwl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwlKitty
+{
+    internal interface IOwl
+    {
+        //Methods from class Owl i want to use
+        string Feathers();
+        int Wings();
+        string Nature();
+
+    }
+}
